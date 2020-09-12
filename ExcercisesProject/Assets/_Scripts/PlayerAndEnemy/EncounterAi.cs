1ee1d00da6a1cae0ba90494ba290f1af87b2524e
Blair-White <blairjwhite@hotmail.com>
2020-09-12 09:34:52 -0400

    EnemyAnimations Combat System work
    Finished Enemy Animations.
    Working on Combat System still....

--- ExcercisesProject/Assets/_Scripts/PlayerAndEnemy/EncounterAi.cs
+++ ExcercisesProject/Assets/_Scripts/PlayerAndEnemy/EncounterAi.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EncounterAi : MonoBehaviour
 {
     [SerializeField]
-    private GameObject Highlighter, Dialogue;
+    private GameObject Highlighter, Dialogue, HealthBar, GameController;
 
+    [SerializeField]
+    private Animator mAnimator;
+
+    public enum States { Inactive, Idle, Acting, Finished }
+    public States State;
+
+    private int delay;
+    private bool Healing, Damaging;
+    private int HealCharges;//0 Attack, 1 heal, 2 Escape;
+    private float RNG, HP, TargetHP;
+    
     //Incoming Messages
     void StartTurn()
     {
@@ -17,12 +29,134 @@ public class EncounterAi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        HP = 1.0f;
+        TargetHP = 1.0f;
+        HealCharges = 1;
+        GameController = GameObject.Find("EncounterController");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Healing)
+        { 
+            if(TargetHP < HP)
+            {
+                TargetHP += 0.1f;
+            }
+            if(TargetHP == HP)
+            {
+                Healing = false;
+            }
+        }
+
+        if (Damaging)
+        {
+            if(TargetHP > HP)
+            {
+                TargetHP -= .01f;
+            }
+            if (TargetHP == HP)
+            {
+                Damaging = false;
+            }
+        }
+
+        if(HP <= 0)
+        {
+            GameController.SendMessage("EnemyKilled");
+        }
+
+        switch (State)
+        {
+            case States.Inactive:
+                break;
+
+            case States.Idle:
+                delay++;
+                if (delay > 180)
+                {
+                    if(HP < .5f)
+                    {
+                        if(HealCharges > 0)
+                        {
+                            Heal();
+                            State = States.Acting;
+                        }
+                        else
+                        {
+                            Escape();
+                            State = States.Acting;
+                        }
+                    }
+                    else
+                    {
+                        Attack();
+                        State = States.Acting;
+                    }
+                    delay = 0;
+                }
+                break;
+
+            case States.Acting:
+                delay++;
+                if (delay > 280)
+                {
+                    State = States.Finished;
+                }
+                break;
+
+            case States.Finished:
+                Highlighter.SetActive(false);
+                ClearDialogue();
+                State = States.Inactive;
+                break;
+
+            default:
+                break;
+        }
     }
+
+    void IncomingDamage()
+    {
+        //temp set damage amount
+        Damaging = true;
+        HP -=.35f;
+    }
+    void Attack() 
+    {
+        State = States.Acting;
+        mAnimator.SetBool("StartAttack", true); 
+    }
+    void EndAttack() 
+    {
+        State = States.Finished;
+        mAnimator.SetBool("StartAttack", false); 
+    }
+    void Heal()
+    {
+        mAnimator.SetBool("StartHeal", true); 
+    
+    }
+    void EndHeal()
+    {
+        mAnimator.SetBool("StartHeal", false); 
+    
+    }
+    void Escape() 
+    {
+        mAnimator.SetBool("StartEscape", true); 
+    
+    }
+    void EndEscape()
+    {
+        mAnimator.SetBool("StartEscape", false); 
+    
+    }
+
+    void ClearDialogue()
+    {
+        Dialogue.GetComponent<Text>().text = "";
+    }
+
 }
--- ExcercisesProject/Assets/_Scripts/PlayerAndEnemy/EncounterPlayer.cs
+++ ExcercisesProject/Assets/_Scripts/PlayerAndEnemy/EncounterPlayer.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EncounterPlayer : MonoBehaviour
 {
@@ -9,11 +11,17 @@ public class EncounterPlayer : MonoBehaviour
 
     [SerializeField]
     private Animator mAnimator;
+
+    public enum States { Inactive, Idle, Acting, Finished }
+    public States State;
+
+    private int delay;
+    private float RNG;
+
     //Incoming Messages
     void StartTurn() 
     {
         Highlighter.SetActive(true);
-        Dialogue.SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
@@ -24,16 +32,78 @@ public class EncounterPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (State)
+        {
+            case States.Inactive:
+                break;
+
+            case States.Idle:
+                break;
+
+            case States.Acting:
+                delay++;
+                if (delay > 280)
+                {
+                    State = States.Finished;
+                }
+                    break;
+
+            case States.Finished:
+                Highlighter.SetActive(false);
+                ClearDialogue();
+                State = States.Inactive;
+                break;
+            
+            default:
+                break;
+        }
+
     }
 
-    void Attack() { mAnimator.SetBool("StartAttack", true); }
-    void EndAttack() { mAnimator.SetBool("StartAttack", false); }
-    void Defend() { mAnimator.SetBool("StartDefend", true); }
-    void EndDefend() { mAnimator.SetBool("StartDefend", false); }
-    void Struggle() { mAnimator.SetBool("StartStruggle", true); }
-    void EndStruggle() { mAnimator.SetBool("StartStruggle", false); }
-    void Escape() { mAnimator.SetBool("StartEscape", true); }
-    void EndEscape() { mAnimator.SetBool("StartEscape", false); }
+    void Attack() 
+    {
+        State = States.Acting;
+        mAnimator.SetBool("StartAttack", true);
+        Dialogue.GetComponent<TextMeshProUGUI>().text = "You Attack The Slime!!";
+    }
+    void EndAttack() 
+    {
+        mAnimator.SetBool("StartAttack", false);
+    }
+    void Defend() 
+    {
+        State = States.Acting;
+        mAnimator.SetBool("StartDefend", true);
+        Dialogue.GetComponent<TextMeshProUGUI>().text = "You Brace For An Attack.";
+    }
+    void EndDefend() 
+    {
+        mAnimator.SetBool("StartDefend", false);
+    }
+    void Struggle()
+    {
+        State = States.Acting;
+        mAnimator.SetBool("StartStruggle", true);
+        Dialogue.GetComponent<TextMeshProUGUI>().text = "You Attempt A Rear Naked Choke.";
+    }
+    void EndStruggle() 
+    {
+        mAnimator.SetBool("StartStruggle", false);
+    }
+    void Escape() 
+    {
+        State = States.Acting;
+        mAnimator.SetBool("StartEscape", true);
+        Dialogue.GetComponent<TextMeshProUGUI>().text = "You Try To Run Away.";
+    }
+    void EndEscape()
+    {
+        mAnimator.SetBool("StartEscape", false);
+    }
+
+    void ClearDialogue() 
+    {
+        Dialogue.GetComponent<TextMeshProUGUI>().text = "";
+    }
 
 }
